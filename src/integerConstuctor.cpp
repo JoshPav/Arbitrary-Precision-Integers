2e36007b5c54ee8e84e6c792c46effdd31925d7e
Tom Webb <n0682255@my.ntu.ac.uk>
2019-05-04 18:24:49 +0100

    updated git ignore as it wasnt ignoring executables

--- src/integerConstuctor.cpp
+++ src/integerConstuctor.cpp
@@ -3,6 +3,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include "integer.h"
+#include <stdexcept>
 
 using namespace ExactArithmetic;
 
@@ -12,6 +13,25 @@ BOOST_AUTO_TEST_CASE(useDefaultConstructor){
   Integer newInt;
   std::string s = newInt.toString();
   BOOST_CHECK_EQUAL(s, "0");
+}
+
+BOOST_AUTO_TEST_CASE(minumum){
+  Integer newInt(0);
+  std::string s = newInt.toString();
+  BOOST_CHECK_EQUAL(s, "0");
+}
+
+BOOST_AUTO_TEST_CASE(maximum){
+  Integer newInt(18446744073709551);
+  std::string s = newInt.toString();
+  BOOST_CHECK_EQUAL(s, "18446744073709551");
+}
+BOOST_AUTO_TEST_CASE(lessThanMinumum){
+  BOOST_CHECK_THROW(Integer(-1), std::invalid_argument);
+}
+
+BOOST_AUTO_TEST_CASE(greaterThanMaximum){
+  BOOST_CHECK_THROW(Integer(18446744073709552), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
