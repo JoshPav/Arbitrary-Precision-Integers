895a64996a265bbd914736940eefb21890a37ac7
Tom Webb <n0682255@my.ntu.ac.uk>
2019-05-05 16:52:48 +0100

    Added testing files to track and updated gitignore to ignore thier executables

---
+++ src/arithmeticTests.cpp
@@ -0,0 +1,29 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE IntegerTests
+#include <boost/test/unit_test.hpp>
+
+#include "integer.h"
+#include <stdexcept>
+
+using namespace ExactArithmetic;
+
+
+BOOST_AUTO_TEST_SUITE(arithmeticTests)
+
+BOOST_AUTO_TEST_SUITE(additionTests)
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(subtractionTests)
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(multiplicationTests)
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(divisionTests)
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
